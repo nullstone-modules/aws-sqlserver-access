b07516a9f43831cc872abcf613966c1d7f586f29
Scott Sickles <scottsickles@gmail.com>
2022-07-27 13:10:55 -0400

    updated to exclude db-admin related items

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,9 @@ output "env" {
       name  = "SQLSERVER_HOST"
       value = local.db_subdomain
     },
+    /*
+    These are commented out because we haven't built the db-admin functionality for sql server yet
+    So without the db-admin, these values aren't generated and granted access
     {
       name  = "SQLSERVER_USER"
       value = local.username
@@ -12,9 +15,13 @@ output "env" {
       name  = "SQLSERVER_DB"
       value = local.database_name
     }
+    */
   ]
 }
 
+/*
+These are commented out because we haven't built the db-admin functionality for sql server yet
+So without the db-admin, these values aren't generated and granted access
 output "secrets" {
   value = [
     {
@@ -23,7 +30,12 @@ output "secrets" {
     },
     {
       name  = "SQLSERVER_URL"
-      value = "sqlserver://${urlencode(local.username)}:${urlencode(random_password.this.result)}@${local.db_endpoint}/${urlencode(local.database_name)}"
+      value = "mssql://${local.db_endpoint}/${urlencode(local.database_name)}:${local.db_port};user=${urlencode(local.username)};password=${urlencode(random_password.this.result)};encrypt=true"
+    },
+    {
+      name  = "SQLSERVER_DSN"
+      value = "Data Source=${local.db_endpoint};Initial Catalog=${urlencode(local.database_name)};User Id=${urlencode(local.username)};Password=${urlencode(random_password.this.result)};Encrypt=true"
     }
   ]
 }
+*/
--- sqlserver.tf
+++ sqlserver.tf
@@ -9,5 +9,4 @@ locals {
   db_subdomain         = split(":", local.db_endpoint)[0]
   db_port              = split(":", local.db_endpoint)[1]
   db_security_group_id = data.ns_connection.sqlserver.outputs.db_security_group_id
-  db_admin_func_name   = data.ns_connection.sqlserver.outputs.db_admin_function_name
 }
