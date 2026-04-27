a004a6f751b2ba9803829df86ee417f9903e8d2b
Brad Sickles <brad.sickles@gmail.com>
2026-04-27 13:42:19 -0400

    Added support for no db security group

--- security.tf
+++ security.tf
@@ -1,17 +1,33 @@
 resource "aws_security_group_rule" "app-to-datastore" {
   security_group_id        = local.security_group_id
+  description              = "Allow access to database ${local.db_endpoint}"
   type                     = "egress"
   from_port                = local.db_port
   to_port                  = local.db_port
   protocol                 = "tcp"
   source_security_group_id = local.db_security_group_id
+
+  count = local.db_security_group_id == "" ? 0 : 1
+}
+
+resource "aws_security_group_rule" "app-to-unspecified-datastore" {
+  security_group_id = local.security_group_id
+  description       = "Allow access to database ${local.db_endpoint}"
+  type              = "egress"
+  from_port         = local.db_port
+  to_port           = local.db_port
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "datastore-from-app" {
   security_group_id        = local.db_security_group_id
+  description              = "Allow ${local.block_name} to access database"
   type                     = "ingress"
   from_port                = local.db_port
   to_port                  = local.db_port
   protocol                 = "tcp"
   source_security_group_id = local.security_group_id
+
+  count = local.db_security_group_id == "" ? 0 : 1
 }
