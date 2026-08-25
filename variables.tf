3343789e86b4fd24e1f59666ee1d06d25a326e38
Brad Sickles <brad.sickles@gmail.com>
2026-08-25 16:34:53 -0400

    Added support for interpolation in var.database_name

--- variables.tf
+++ variables.tf
@@ -10,8 +10,26 @@ EOF
 
 variable "database_name" {
   type        = string
-  description = "Name of database to create in Sql Server. If left blank, uses app name."
+  description = <<EOF
+Name of database to create in Sql Server. If left blank, uses app name.
+The following identifiers are supported for interpolation:
+  {{ NULLSTONE_STACK }}
+  {{ NULLSTONE_BLOCK }}
+  {{ NULLSTONE_ENV }}
+EOF
   default     = ""
+}
+
+// We are using ns_env_variables to interpolate database_name
+data "ns_env_variables" "db_name" {
+  input_env_variables = tomap({
+    NULLSTONE_STACK = local.stack_name
+    NULLSTONE_BLOCK = local.block_name
+    NULLSTONE_APP   = local.block_name
+    NULLSTONE_ENV   = local.env_name
+    DATABASE_NAME   = coalesce(var.database_name, local.block_name)
+  })
+  input_secrets = tomap({})
 }
 
 variable "trust_server_certificate" {
@@ -29,7 +47,7 @@ variable "persist_security_info" {
 locals {
   security_group_id = var.app_metadata["security_group_id"]
   username          = local.resource_name
-  database_name     = coalesce(var.database_name, local.block_name)
+  database_name     = data.ns_env_variables.db_name.env_variables["DATABASE_NAME"]
 
   extra_dsn = join("", compact([
     var.trust_server_certificate ? ";TrustServerCertificate=True" : "",
